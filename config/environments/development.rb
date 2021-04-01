bbc7fd1b0fd47000c3ce6a7e6b196ffb2a0147d7
Germain Carré <git@germain.sh>
2021-04-01 17:37:36 +0200

    Adding user count on homepage

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,9 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  # Allow any hostname in DEV
+  config.hosts = nil
+
   # Do not eager load code on boot.
   config.eager_load = false
 
