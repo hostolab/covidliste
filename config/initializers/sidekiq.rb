3e94118787060d2eae23d7f9ef40a5d552b80921
Mathieu Ripert <mathieu@instacart.com>
2021-04-02 15:22:42 +0200

    disable ssl for sidekiq (#32)

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_client do |config|
-  config.redis = { url: (ENV['REDIS_URL'] || 'redis://localhost:6379'), size: 1 }
+  config.redis = { url: (ENV['REDIS_URL'] || 'redis://localhost:6379'), size: 1, ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: (ENV['REDIS_URL'] || 'redis://localhost:6379'), size: 20 }
+  config.redis = { url: (ENV['REDIS_URL'] || 'redis://localhost:6379'), size: 20, ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
 end
