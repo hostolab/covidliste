c908bc063b148725380067a1d72e34524787f86e
Mathieu Ripert <mathieu@instacart.com>
2021-04-02 08:20:47 +0200

    improves signup experience

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -13,7 +13,7 @@ class ConfirmationsController < ::Devise::ConfirmationsController
 
   def after_confirmation_path_for(_resource_name, resource)
     token = resource.send(:set_reset_password_token)
-    root_path
+    edit_password_url(resource, reset_password_token: token)
   end
 
 end
