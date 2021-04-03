5fff3800f3bc67f1119bdb86a3f43a3090e909a2
Mathieu Ripert <mathieu@instacart.com>
2021-04-03 13:37:20 +0200

    [Devise] if user already confirmed, do go to reset password page

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -3,9 +3,12 @@ class ConfirmationsController < ::Devise::ConfirmationsController
   def show
     self.resource = resource_class.confirm_by_token(params[:confirmation_token])
     yield resource if block_given?
-    if resource.errors.empty? || resource.confirmed?
+    if resource.errors.empty?
       set_flash_message!(:notice, :confirmed)
       respond_with_navigational(resource){ redirect_to after_confirmation_path_for(resource_name, resource) }
+    elsif resource.confirmed?
+      set_flash_message!(:notice, :already_confirmed)
+      respond_with_navigational(resource.errors, status: :unprocessable_entity){ redirect_to :new_user_password }
     else
       respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
     end
