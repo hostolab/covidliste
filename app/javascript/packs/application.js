03567cd7b6a9ecfc3c576e49e21423435de7dd8d
Germain Carré <git@germain.sh>
2021-04-03 21:04:04 +0200

    Admin search area (#64)
    Co-authored-by: Coline Juillerat <coline.ju@gmail.com>

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -22,8 +22,10 @@ ActiveStorage.start()
 import "bootstrap";
 
 import { placesAutocomplete } from '../plugins/places_autocomplete';
+import { leafletMap } from '../plugins/leaflet_map';
 document.addEventListener('turbolinks:load', () => {
   const appId = process.env.PLACES_APP_ID;
   const apiKey = process.env.PLACES_API_KEY;
   placesAutocomplete(appId, apiKey);
+  leafletMap();
 });
