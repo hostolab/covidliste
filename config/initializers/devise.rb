3fd31869ef6727f592e20aaca74cc87f31c647ea
Mathieu Ripert <mathieu@instacart.com>
2021-04-03 14:31:58 +0200

    [Devise] confirm email within 1 day

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -152,7 +152,7 @@ Devise.setup do |config|
   # their account can't be confirmed with the token any more.
   # Default is nil, meaning there is no restriction on how long a user can take
   # before confirming their account.
-  # config.confirm_within = 3.days
+  config.confirm_within = 1.days
 
   # If true, requires any email changes to be confirmed (exactly the same way as
   # initial account confirmation) to be applied. Requires additional unconfirmed_email
