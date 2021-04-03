c2facf1b7d99b63086860f2b5cfafd6c02db4830
Pierre Fournier <peiofour@gmail.com>
2021-04-03 16:39:01 +0200

    Init faq page #48

--- config/routes.rb
+++ config/routes.rb
@@ -40,7 +40,8 @@ Rails.application.routes.draw do
   ## pages
   get '/mentions_legales' => "pages#mentions_legales", as: :mentions_legales
   get '/privacy' => "pages#privacy", as: :privacy
-
+  get '/faq' => "pages#faq", as: :faq
+  
   root to: 'users#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
