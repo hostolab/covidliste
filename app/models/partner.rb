6cfb8432b7807253f14a8fd29a65bd1df97ffe55
Coline Juillerat <68182973+colinemarie@users.noreply.github.com>
2021-04-03 15:16:33 +0200

    Issue#8 VaccinationCenter and Partner models (#56)
    * create partner devise model
    
    * vaccination center model created

---
+++ app/models/partner.rb
@@ -0,0 +1,11 @@
+class Partner < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable,
+         :recoverable,
+         :rememberable,
+         :validatable,
+         :registerable
+
+  validates :name, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_03_061510) do
+ActiveRecord::Schema.define(version: 2021_04_03_113911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,19 @@ ActiveRecord::Schema.define(version: 2021_04_03_061510) do
     t.index ["creator_id"], name: "index_blazer_queries_on_creator_id"
   end
 
+  create_table "partners", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.index ["email"], name: "index_partners_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_partners_on_reset_password_token", unique: true
+  end
+
   create_table "roles", force: :cascade do |t|
     t.string "name"
     t.string "resource_type"
@@ -112,4 +125,24 @@ ActiveRecord::Schema.define(version: 2021_04_03_061510) do
     t.index ["user_id"], name: "index_users_roles_on_user_id"
   end
 
+  create_table "vaccination_centers", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "address"
+    t.float "lat"
+    t.float "lon"
+    t.string "kind"
+    t.boolean "pfizer"
+    t.boolean "moderna"
+    t.boolean "astrazeneca"
+    t.boolean "janssen"
+    t.date "confirmed_at"
+    t.string "phone_number"
+    t.bigint "partner_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["partner_id"], name: "index_vaccination_centers_on_partner_id"
+  end
+
+  add_foreign_key "vaccination_centers", "partners"
 end
