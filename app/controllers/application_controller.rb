2bf7d4dec2db8fc73fcb943de074db4742b9e956
Maxence Aïci <maxence.aici@alan.eu>
2021-04-04 20:22:04 +0200

    Only apply strict CSP for non-admin app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  content_security_policy do |policy|
+    policy.default_src :self, :https
+    policy.font_src    :self, :data, :https
+    policy.img_src     :self, :data, :https
+    policy.object_src  :none
+    policy.script_src  :strict_dynamic
+    policy.style_src   :self, :https, :unsafe_inline
+  end
 end
