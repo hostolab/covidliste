b64a606f60dd6c750d0d23f0925ccae79d4f92af
Mathieu Ripert <mathieu@instacart.com>
2021-04-04 20:53:05 +0200

    [Chiffrement] drop email column (#83)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_04_162352) do
+ActiveRecord::Schema.define(version: 2021_04_04_184439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,7 +95,6 @@ ActiveRecord::Schema.define(version: 2021_04_04_162352) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
     t.date "birthdate"
     t.float "lat"
     t.float "lon"
@@ -115,7 +114,6 @@ ActiveRecord::Schema.define(version: 2021_04_04_162352) do
     t.text "address_ciphertext"
     t.text "email_ciphertext"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token"
-    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
