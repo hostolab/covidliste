3d338bab4210ae84dc06d4fb4ae3e8de79e707b2
guillaume.wrobel@yespark.fr <guillaume.wrobel@yespark.fr>
2021-04-04 21:08:54 +0200

    grr another typo

--- spec/system/home_spec.rb
+++ spec/system/home_spec.rb
@@ -3,6 +3,6 @@ require "rails_helper"
 RSpec.describe "Home page", type: :system do
   it "loads properly" do
     visit "/"
-    expect(page).to have_text("Qu’est ce que Covidliste ?")
+    expect(page).to have_text("Qu’est-ce que Covidliste ?")
   end
 end
