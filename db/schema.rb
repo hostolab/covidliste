cf94c36059f16b21bf200ac8a42023cd1e27fa33
Mathieu Ripert <mathieu@instacart.com>
2021-04-04 21:04:23 +0200

    add back email to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_04_184439) do
+ActiveRecord::Schema.define(version: 2021_04_04_190311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -113,6 +113,7 @@ ActiveRecord::Schema.define(version: 2021_04_04_184439) do
     t.text "phone_number_ciphertext"
     t.text "address_ciphertext"
     t.text "email_ciphertext"
+    t.string "email"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
