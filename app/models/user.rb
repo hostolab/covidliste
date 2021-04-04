b484142fea7304f2211cefdc7bd79278c8b280bc
Sébastien Saunier <seb@saunier.me>
2021-04-04 23:40:49 +0200

    Ignore email before deletion

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,9 @@ class User < ApplicationRecord
   encrypts :phone_number
   encrypts :email
 
-  blind_index :email, migrating: true
+  blind_index :email
+
+  self.ignored_columns = ['email']
 
   validates :firstname, presence: true
   validates :lastname, presence: true
