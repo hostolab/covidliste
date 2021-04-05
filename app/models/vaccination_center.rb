6bc275a25ea71522075b7238117974f122e65389
Guillaume Wrobel <guillaume.wrobel@yespark.fr>
2021-04-05 17:07:26 +0200

    fix module order (#102)

--- app/models/vaccination_center.rb
+++ app/models/vaccination_center.rb
@@ -1,14 +1,5 @@
 class VaccinationCenter < ApplicationRecord
 
-  validates_presence_of :name, :description, :address, :lat, :lon, :phone_number
-  validates :kind, inclusion: { in: VaccinationCenter::Kinds::ALL }
-
-  has_many :partner_vaccination_center
-  has_many :partner, through: :partner_vaccination_center
-  belongs_to :confirmer, class_name: 'User', optional: true
-
-  scope :confirmed, -> { where.not(confirmed_at: nil) }
-
   module Kinds
     CENTRE_VACCINATION = 'Centre de vaccination'
     CABINET_MEDICAL = 'Cabinet médical'
@@ -17,6 +8,15 @@ class VaccinationCenter < ApplicationRecord
 
     ALL = [CENTRE_VACCINATION, CABINET_MEDICAL, PHARMACIE, EHPAD].freeze
   end
+
+  validates_presence_of :name, :description, :address, :lat, :lon, :phone_number
+  validates :kind, inclusion: { in: VaccinationCenter::Kinds::ALL }
+
+  has_many :partner_vaccination_center
+  has_many :partner, through: :partner_vaccination_center
+  belongs_to :confirmer, class_name: 'User', optional: true
+
+  scope :confirmed, -> { where.not(confirmed_at: nil) }
 
   def confirmed?
     confirmed_at.present?
