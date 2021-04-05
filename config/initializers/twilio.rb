a1773f50edaf9ad020c0963f7f359799e66aeace
Sébastien Saunier <seb@saunier.me>
2021-04-05 15:08:38 +0200

    Indentation

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -1,4 +1,4 @@
 Twilio.configure do |config|
-    config.account_sid = ENV['TWILIO_ACCOUNT_SID']
-    config.auth_token = ENV['TWILIO_AUTH_TOKEN']
-  end
+  config.account_sid = ENV['TWILIO_ACCOUNT_SID']
+  config.auth_token = ENV['TWILIO_AUTH_TOKEN']
+end
