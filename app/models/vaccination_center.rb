c0e98e94bb28a06d6398774c9f491390f90b7128
Guillaume Wrobel <guillaume.wrobel@yespark.fr>
2021-04-05 16:46:33 +0200

    use module for vaccination center types to avoid typo (#101)

--- app/models/vaccination_center.rb
+++ app/models/vaccination_center.rb
@@ -1,13 +1,22 @@
 class VaccinationCenter < ApplicationRecord
 
   validates_presence_of :name, :description, :address, :lat, :lon, :phone_number
-  validates :kind, inclusion: { in: ['Centre de vaccination', 'Cabinet médical', 'Pharmacie', 'Ephad'] }
+  validates :kind, inclusion: { in: VaccinationCenter::Kinds::ALL }
 
   has_many :partner_vaccination_center
   has_many :partner, through: :partner_vaccination_center
   belongs_to :confirmer, class_name: 'User', optional: true
 
   scope :confirmed, -> { where.not(confirmed_at: nil) }
+
+  module Kinds
+    CENTRE_VACCINATION = 'Centre de vaccination'
+    CABINET_MEDICAL = 'Cabinet médical'
+    PHARMACIE = 'Pharmacie'
+    EHPAD = 'Ehpad'
+
+    ALL = [CENTRE_VACCINATION, CABINET_MEDICAL, PHARMACIE, EHPAD].freeze
+  end
 
   def confirmed?
     confirmed_at.present?
