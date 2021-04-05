5ce4ebfcf7c0c619c845e16f311aca732c160a8a
jcsanti <jean.charles.santi@gmail.com>
2021-04-05 22:51:27 +0200

    Remove column "campaign_batches"."expires_at"
    "matches" also has an "expires_at" column meaning we don't need this one.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_05_150350) do
+ActiveRecord::Schema.define(version: 2021_04_05_204958) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,7 +75,6 @@ ActiveRecord::Schema.define(version: 2021_04_05_150350) do
     t.bigint "campaign_id"
     t.bigint "vaccination_center_id"
     t.bigint "partner_id"
-    t.datetime "expires_at", null: false
     t.integer "size", null: false
     t.integer "duration_in_minutes", default: 10, null: false
     t.datetime "created_at", precision: 6, null: false
