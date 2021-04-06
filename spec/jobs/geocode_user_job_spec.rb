98e0507469ecb3a3b8ae2d47d1f64b9374b4ace0
Mathieu Ripert <mathieu.ripert@gmail.com>
2021-04-06 08:35:51 +0200

    [Geocoding] use etalab api

--- spec/jobs/geocode_user_job_spec.rb
+++ spec/jobs/geocode_user_job_spec.rb
@@ -5,30 +5,25 @@ describe GeocodeUserJob do
 
   subject { described_class.perform_now(user.id)}
 
-  before(:each) do
-    Geocoder.configure(:lookup => :test)
-    Geocoder::Lookup::Test.set_default_stub(
-      [
-        {
-          'coordinates'  => [48.8534, 2.3488],
-          'address'      => 'Paris',
-          'country_code' => '75001'
-        }
-      ]
-    )
+  before(:each) do 
+    allow_any_instance_of(GeocodingService).to receive(:geocode).and_return({
+      lat: 48.12345,
+      lon: 2.12345,
+      postal_code: "75001"
+    })
   end
   
   context "user with missing lat/lon" do
-    it "does call geocoder and approximate coords" do
+    it "does call geocoding and approximate coords" do
       subject
       user.reload
-      expect(user.lat.to_f).to eq 48.853
-      expect(user.lon.to_f).to eq 2.349
+      expect(user.lat.to_f).to eq 48.123
+      expect(user.lon.to_f).to eq 2.123
     end
 
     context "user with lat/lon" do
       before { user.update(lat: 55.12345, lon: 1.12345) }
-      it "does not run geocoder but still approximate coords" do
+      it "does not run geocoding but still approximate coords" do
         subject
         user.reload
         expect(user.lat.to_f).to eq 55.123
