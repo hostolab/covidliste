fec8caee02050192e265c562dacd8b40c0010ae7
Sébastien Saunier <seb@saunier.me>
2021-04-06 18:16:40 +0200

    Make standardrb happy

--- db/migrate/20210405205147_add_column_max_age_to_campaigns.rb
+++ db/migrate/20210405205147_add_column_max_age_to_campaigns.rb
@@ -2,6 +2,6 @@ class AddColumnMaxAgeToCampaigns < ActiveRecord::Migration[6.1]
   def change
     add_column :campaigns, :max_age, :integer
 
-    add_check_constraint(:campaigns, 'max_age > 0 AND max_age > min_age', name: 'max_age_gt_zero')
+    add_check_constraint(:campaigns, "max_age > 0 AND max_age > min_age", name: "max_age_gt_zero")
   end
 end
