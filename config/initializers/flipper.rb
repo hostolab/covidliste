e16f2c2d1fe1fbf07fd3c70ae54e97d340c2d8b8
Mathieu Ripert <mathieu.ripert@gmail.com>
2021-04-06 16:49:15 +0200

    [Tools] Setup flipper (#147)
    * [Tools] Setup flipper
    
    * fix linter

---
+++ config/initializers/flipper.rb
@@ -0,0 +1,6 @@
+Flipper.configure do |config|
+  config.default do
+    client = Redis.new(url: ENV["FLIPPER_REDIS_URL"] || ENV["REDIS_URL"])
+    Flipper.new(Flipper::Adapters::Redis.new(client))
+  end
+end
