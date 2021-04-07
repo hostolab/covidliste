45637b8c60bf1da9e0e82dbc86eaf181263f76dd
Coline Juillerat <68182973+colinemarie@users.noreply.github.com>
2021-04-07 19:13:24 +0200

    Update app/models/user.rb
    Co-authored-by: Sébastien Saunier <seb@saunier.me>

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,7 +34,7 @@ class User < ApplicationRecord
     },
     if: :email_changed?
 
-  has_many :matches
+  has_many :matches, dependent: :nullify
 
   after_commit :geocode_address, if: :saved_change_to_address?
   before_save :approximate_coords
