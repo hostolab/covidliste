104cead2357a7f56ce30fdf9d1641f3fffd955b6
guillaume.wrobel@yespark.fr <guillaume.wrobel@yespark.fr>
2021-04-07 11:53:51 +0200

    catch geocoder_user_job with deleted user

--- app/jobs/geocode_user_job.rb
+++ app/jobs/geocode_user_job.rb
@@ -1,8 +1,9 @@
 class GeocodeUserJob < ActiveJob::Base
   queue_as :low
 
-  def perform(user)
-    return if user.address.nil?
+  def perform(user_id)
+    user = User.find_by(id: user_id)
+    return if user.nil? || user.address.nil?
 
     result = GeocodingService.new(user.address).geocode
     user.update(result) if result
--- app/models/user.rb
+++ app/models/user.rb
@@ -50,7 +50,7 @@ class User < ApplicationRecord
   end
 
   def geocode_address
-    GeocodeUserJob.perform_later(self)
+    GeocodeUserJob.perform_later(self.id)
   end
 
   def full_name
