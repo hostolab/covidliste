648b6d31495db83a04efc2536a0b0968409918f5
Guillaume Wrobel <20317297+GuillaumeWrobel@users.noreply.github.com>
2021-04-07 09:40:13 +0200

    Fichier robots.txt pour bloquer l'indexation des pages privées (#166)
    * add robots.txt
    
    * disallow admin
    
    * handle other env than prod
    
    * more apostrophes courbes
    
    * disallow all agents
    
    Co-authored-by: Sébastien Saunier <seb@saunier.me>

--- config/routes.rb
+++ config/routes.rb
@@ -76,5 +76,8 @@ Rails.application.routes.draw do
   get "/privacy" => "pages#privacy", :as => :privacy
   get "/faq" => "pages#faq", :as => :faq
 
+  ## robots.txt
+  get "/robots.txt", to: "pages#robots"
+
   root to: "users#new"
 end
