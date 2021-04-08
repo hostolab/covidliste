2eee794debc8feec1db9ef2b4ba942ba5523b1e4
Sébastien Saunier <seb@saunier.me>
2021-04-08 18:34:48 +0200

    Campaign CSV export should match the HTML view

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -21,15 +21,13 @@ class Campaign < ApplicationRecord
 
   def to_csv
     CSV.generate(headers: true) do |csv|
-      csv << %w[firstname lastname birthdate email phone_number zipcode confirmed_at]
+      csv << %w[firstname lastname birthdate phone_number confirmed_at]
       matches.confirmed.order(:confirmed_at).each do |match|
         csv << [
           match.user.firstname,
           match.user.lastname,
           match.user.birthdate,
-          match.user.email,
           match.user.phone_number,
-          match.user.zipcode,
           match.confirmed_at
         ]
       end
