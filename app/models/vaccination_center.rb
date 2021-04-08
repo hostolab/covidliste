fae8a92e79636d339c099d76aef385932298337a
Maxence Aïci <maxence.aici@alan.eu>
2021-04-08 10:35:41 +0200

    Add campaign reach simulation API (#198)
    * Add campaign reach simulation backend
    
    * Update config/routes.rb
    
    * SimulusJS conroller on campaign#new to simulate number of people reached
    
    Co-authored-by: Sébastien Saunier <seb@saunier.me>

--- app/models/vaccination_center.rb
+++ app/models/vaccination_center.rb
@@ -82,6 +82,17 @@ class VaccinationCenter < ApplicationRecord
     end
   end
 
+  def reachable_users_query(min_age:, max_age:, max_distance_in_meters:, limit: nil)
+    User
+      .joins("LEFT JOIN matches ON matches.user_id = users.id")
+      .where("SQRT(((? - lat)*110.574)^2 + ((? - lon)*111.320*COS(lat::float*3.14159/180))^2) < ?", lat, lon, max_distance_in_meters / 1000)
+      .where("(matches.expires_at < now()::date AND matches.confirmed_at IS NULL) OR matches.id IS NULL")
+      .where("(DATE_PART('year', now()::date) - DATE_PART('year', birthdate::date))::int >= ? and (DATE_PART('year', now()::date) - DATE_PART('year', birthdate::date))::int <= ?", min_age, max_age)
+      .where("users.confirmed_at IS NOT NULL")
+      .order(id: :asc)
+      .limit(limit)
+  end
+
   private
 
   def push_to_slack
