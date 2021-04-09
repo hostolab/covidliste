28bad45e2b5d16f816786556f7cf5cb0ecd273b4
Sébastien Saunier <seb@saunier.me>
2021-04-09 14:37:56 +0200

    Proper property

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -9,7 +9,7 @@ class Campaign < ApplicationRecord
 
   enum status: {running: 0, completed: 1, canceled: 2}
 
-  validates :available_doses, numericality: {greater_than: 0, lesser_than_or_equal_to: MAX_DOSES}
+  validates :available_doses, numericality: {greater_than: 0, less_than_or_equal_to: MAX_DOSES}
   validates :vaccine_type, presence: true
   validates :min_age, numericality: {greater_than: 17}
   validates :max_age, numericality: {greater_than: 17}
