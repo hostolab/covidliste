4e211dc97795805d1fbe3d964509b6e3a065bf6e
ManyBaby <hello@hacker-vaillant.org>
2021-04-10 16:09:24 +0200

    feat: :sparkles: New partial for overbooked dose

--- config/application.rb
+++ config/application.rb
@@ -2,11 +2,11 @@ require_relative "boot"
 
 require "rails/all"
 require "csv"
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+Dotenv::Railtie.load
 module Covidliste
   class Application < Rails::Application
     config.generators do |generate|
