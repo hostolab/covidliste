58c610680a01207db877d6af204d40f1dfd8bdf1
EmCousin <emmanuel@braidio.com>
2021-04-11 15:37:59 -0300

    Use phonelib to enforce phone number validation

--- app/models/concerns/has_phone_number_concern.rb
+++ app/models/concerns/has_phone_number_concern.rb
@@ -4,45 +4,22 @@ module HasPhoneNumberConcern
   extend ActiveSupport::Concern
 
   included do
-    before_validation :format_phone_number
-    validates :phone_number, length: {minimum: 7, maximum: 15, allow_blank: false}
+    before_validation :format_phone_number, if: :phone_number?
+    validates :phone_number,
+      phone: {
+        possible: true,
+        types: %i[mobile voip],
+        allow_blank: false
+      }
   end
 
   def human_friendly_phone_number
-    phone_number.gsub(/^33/, "0").match(/(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})/)&.captures&.join(" ")
+    Phonelib.parse(phone_number).national
   end
 
   private
 
   def format_phone_number
-    return if phone_number.blank?
-
-    new_phone = phone_number.to_s
-
-    # Remove separators and other unicode characters
-    # 06  12-23.4.5 67-8
-    # => 0612345678
-    patterns = [" ", "+", "-", ".", ",", "/", "－", "–", "_", "\u00A0", "\u202C", "\u202D", "\u0020", "~", "—", "@",
-      "*", "	", "\t"]
-    patterns.each do |string|
-      new_phone = new_phone.gsub(string, "")
-    end
-
-    # Remove two leading zeros
-    # 00336xx
-    # => 336xx
-    new_phone = new_phone[2..] if new_phone.length > 2 && new_phone[0..1] == "00"
-
-    # French mobile and fix numbers have 10 digits, and we want a 33x number
-    # 0612345678
-    # => 33612345678
-    if new_phone.length == 10 && new_phone[0..0] == "0"
-      # "FR 06 xx xx xx xx"
-      # "FR 07 xx xx xx xx"
-      # "FR 01 xx xx xx xx"
-      new_phone = "33" + new_phone[1..]
-    end
-
-    self.phone_number = new_phone
+    self.phone_number = Phonelib.parse(phone_number).full_e164
   end
 end
--- spec/support/has_phone_number_spec.rb
+++ spec/support/has_phone_number_spec.rb
@@ -9,7 +9,7 @@ RSpec.shared_examples "has phone number" do |param|
     subject { model.human_friendly_phone_number }
 
     it "formats the phone number for the view" do
-      allow(model).to receive(:phone_number).and_return("33612345678")
+      allow(model).to receive(:phone_number).and_return("+33612345678")
       expect(subject).to eq "06 12 34 56 78"
     end
   end
