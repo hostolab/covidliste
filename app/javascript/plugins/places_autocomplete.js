ed2b2841b7f26617906cc50f162c465a87c8da0b
Mathieu Ripert <mathieu.ripert@gmail.com>
2021-04-11 22:22:52 +0200

    [Geocoding] remove unused code to get lat/lon for users

--- app/javascript/plugins/places_autocomplete.js
+++ app/javascript/plugins/places_autocomplete.js
@@ -1,13 +1,11 @@
 import places from "places.js";
 
 const placesAutocomplete = (appId, apiKey) => {
+  
+  // User Form
   const addressInput = document.getElementById("user_address");
-  const latInput = document.getElementById("user_lat");
-  const lonInput = document.getElementById("user_lon");
-  const zipcodeInput = document.getElementById("user_zipcode");
-  const cityInput = document.getElementById("user_city");
   if (addressInput) {
-    let p = places({
+    places({
       container: addressInput,
       appId: appId,
       apiKey: apiKey,
@@ -15,32 +13,9 @@ const placesAutocomplete = (appId, apiKey) => {
       language: "fr",
       countries: ["fr"],
     });
-    p.on("change", function (e) {
-      let latlng = e.suggestion.latlng;
-      latInput.value = latlng["lat"];
-      lonInput.value = latlng["lng"];
-      zipcodeInput.value = e.suggestion.postcode;
-      cityInput.value = e.suggestion.city;
-    });
   }
-  const addressInputMap = document.getElementById("map_address");
-  const latInputMap = document.getElementById("map_lat");
-  const lonInputMap = document.getElementById("map_lon");
-  if (addressInputMap) {
-    let p = places({
-      container: addressInputMap,
-      appId: appId,
-      apiKey: apiKey,
-    }).configure({
-      language: "fr",
-      countries: ["fr"],
-    });
-    p.on("change", function (e) {
-      let latlng = e.suggestion.latlng;
-      latInputMap.value = latlng["lat"];
-      lonInputMap.value = latlng["lng"];
-    });
-  }
+
+  // Vaccination Center Signup Form
   const centerAddressInput = document.getElementById(
     "vaccination_center_address"
   );
