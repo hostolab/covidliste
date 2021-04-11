4ef267bb844568c828cfcc96274eb7cfd9bcb0b7
ManyBaby <hello@hacker-vaillant.org>
2021-04-11 10:59:20 +0200

    test: :white_check_mark: Testing human friendly phone number

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -27,6 +27,8 @@ if ENV["CI"] == "true"
   SimpleCov.formatter = SimpleCov::Formatter::Codecov
 end
 
+Dir["./spec/support/**/*.rb"].sort.each { |f| require f }
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
