e673c2c241ec338b4d2cc90f83b7821f6bb16e74
Pierre Fournier <peiofour@gmail.com>
2021-04-13 19:56:10 +0200

    Ajout contrainte age astrazeneca #347

--- app/javascript/controllers/new_campaign_controller.js
+++ app/javascript/controllers/new_campaign_controller.js
@@ -42,6 +42,11 @@ export default class extends Controller {
       return;
     }
 
+    if (vaccineType === 'astrazeneca' && (minAge < 55 || maxAge < 55)){
+      this.simulationResultTarget.innerHTML = "Simulation impossible, l'âge minimum requis pour le vaccin AstraZeneca est de 55 ans."
+      return
+    }
+
     fetch(this.simulatePathValue, {
       method: "POST",
       credentials: "same-origin",
