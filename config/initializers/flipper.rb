6d51e8078acb4da6731a5bb40b0eb24a66408e00
gearnode <bryan@frimin.fr>
2021-04-13 13:46:47 +0200

    Remove unused flipper dependency

--- config/initializers/flipper.rb
+++
@@ -1,6 +0,0 @@
-Flipper.configure do |config|
-  config.default do
-    client = Redis.new(url: ENV["FLIPPER_REDIS_URL"] || ENV["REDIS_URL"])
-    Flipper.new(Flipper::Adapters::Redis.new(client))
-  end
-end
