7ca0f258da61d04543b84d228121f43350c52b7b
Mathieu Ripert <mathieu.ripert@gmail.com>
2021-04-13 11:55:23 +0200

    Cron jobs for blazer checks (#343)
    * Cron jobs for blazer checks
    
    * prettier

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,3 +5,9 @@ end
 Sidekiq.configure_server do |config|
   config.redis = {url: (ENV["REDIS_URL"] || "redis://localhost:6379"), size: 20}
 end
+
+schedule_file = "config/schedule.yml"
+
+if File.exist?(schedule_file) && Sidekiq.server?
+  Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+end
