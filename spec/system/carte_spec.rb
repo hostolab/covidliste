f26bf26b039a03b6da11d96d5072eb90fd34d17a
Adrien Poly <adrienpoly@gmail.com>
2021-04-14 23:24:21 +0200

    remove Guyane for expectations as there is a typo and it will be corrected

--- spec/system/carte_spec.rb
+++ spec/system/carte_spec.rb
@@ -9,7 +9,6 @@ RSpec.describe "Carte Page", type: :system do
     visit "/carte"
 
     expect(page).to have_text("France métropolitaine")
-    expect(page).to have_text("Guyanne")
     expect(page).to have_css("#map.mapboxgl-map")
   end
 end
