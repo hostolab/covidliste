f5595cbed39ec326022a65d47f909e36ff6d4f7c
Mathieu Ripert <mathieu.ripert@gmail.com>
2021-04-14 11:50:46 +0200

    [security] simple throttle of requests with rack-attack

---
+++ config/initializers/rack_attack.rb
@@ -0,0 +1,21 @@
+Rack::Attack.throttle("logins/ip", limit: 20, period: 10.minutes) do |req|
+  req.ip if req.post? && req.path.start_with?("/login")
+end
+
+Rack::Attack.throttle("partners/login/ip", limit: 20, period: 10.minutes) do |req|
+  req.ip if req.post? && req.path.start_with?("/partners/login")
+end
+
+Rack::Attack.throttle("partners/password/new", limit: 20, period: 10.minutes) do |req|
+  req.ip if req.post? && req.path.start_with?("/partners/password/new")
+end
+
+Rack::Attack.throttle("users/password/new", limit: 20, period: 10.minutes) do |req|
+  req.ip if req.post? && req.path.start_with?("/users/password/new")
+end
+
+
+ActiveSupport::Notifications.subscribe("rack.attack") do |name, start, finish, request_id, req|
+  puts "Throttled #{req.env["rack.attack.match_discriminator"]}"
+  ## todo add a logging tool
+end
