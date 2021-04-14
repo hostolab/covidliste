b789635583b75aee90dd03dc2ae8701479a97b1a
Romain Champourlier <public@rchampourlier.com>
2021-04-14 17:20:03 +0200

    Revert "Prompt user to check Age & Name when matched (#379)"
    This reverts commit 92dc4ea7e752f08acef32ca64abcd52ace4fae8d.

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -2,10 +2,6 @@ class MatchesController < ApplicationController
   before_action :set_match, only: [:show, :update]
 
   def show
-    if @match.age != @match.user.age
-      flash[:error] = "Désolé, votre âge a changé depuis l'envoi de la notification. Pas d'inquiétude, vous restez dans notre liste de volontaires ! Vous serez contacté dès qu'une dose est à nouveau disponible près de chez vous."
-      redirect_to root_path
-    end
   end
 
   def update
