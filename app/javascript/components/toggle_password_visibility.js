5dcb88b2a77c7566efc04ed95ea16d806259d78b
guillaume.wrobel@yespark.fr <guillaume.wrobel@yespark.fr>
2021-04-15 21:03:40 +0200

    prettier

--- app/javascript/components/toggle_password_visibility.js
+++ app/javascript/components/toggle_password_visibility.js
@@ -1,15 +1,21 @@
-var input = document.querySelector("[data-behavior='toggle-password-visibility'] input");
-var icon = document.querySelector("[data-behavior='toggle-password-visibility'] i");
+var input = document.querySelector(
+  "[data-behavior='toggle-password-visibility'] input"
+);
+var icon = document.querySelector(
+  "[data-behavior='toggle-password-visibility'] i"
+);
 
-document.querySelector("[data-behavior='toggle-password-visibility'] a").addEventListener("click", function (e) {
-  if (input.getAttribute("type") === "text") {
-    input.setAttribute("type", "password");
-    icon.classList.add("fa-eye-slash");
-    icon.classList.remove("fa-eye");
-  } else if (input.getAttribute("type") === "password") {
-    input.setAttribute("type", "text");
-    icon.classList.add("fa-eye");
-    icon.classList.remove("fa-eye-slash");
-  }
-  e.preventDefault();
-});
+document
+  .querySelector("[data-behavior='toggle-password-visibility'] a")
+  .addEventListener("click", function (e) {
+    if (input.getAttribute("type") === "text") {
+      input.setAttribute("type", "password");
+      icon.classList.add("fa-eye-slash");
+      icon.classList.remove("fa-eye");
+    } else if (input.getAttribute("type") === "password") {
+      input.setAttribute("type", "text");
+      icon.classList.add("fa-eye");
+      icon.classList.remove("fa-eye-slash");g
+    }
+    e.preventDefault();
+  });
