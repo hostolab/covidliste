30de80cf1504d8d3187153cf5eadba79904acb04
guillaume.wrobel@yespark.fr <guillaume.wrobel@yespark.fr>
2021-04-15 19:18:36 +0200

    add point

--- app/javascript/components/toggle_password_visibility.js
+++ app/javascript/components/toggle_password_visibility.js
@@ -1,9 +1,14 @@
-$(".show_hide_password a").on("click", function(e) {
-  return e.preventDefault(),
-    "text" === $(".show_hide_password input").attr("type") ? ($(".show_hide_password input").attr("type", "password"),
-      $(".show_hide_password i").addClass("fa-eye-slash"),
-      $(".show_hide_password i").removeClass("fa-eye")) : "password" === $(".show_hide_password input").attr("type") ? ($(".show_hide_password input").attr("type", "text"),
-      $(".show_hide_password i").removeClass("fa-eye-slash"),
-      $(".show_hide_password i").addClass("fa-eye")) : void 0
+$(".show_hide_password a").on("click", function (e) {
+  return (
+    e.preventDefault(),
+    "text" === $(".show_hide_password input").attr("type")
+      ? ($(".show_hide_password input").attr("type", "password"),
+        $(".show_hide_password i").addClass("fa-eye-slash"),
+        $(".show_hide_password i").removeClass("fa-eye"))
+      : "password" === $(".show_hide_password input").attr("type")
+      ? ($(".show_hide_password input").attr("type", "text"),
+        $(".show_hide_password i").removeClass("fa-eye-slash"),
+        $(".show_hide_password i").addClass("fa-eye"))
+      : void 0
+  );
 });
-
