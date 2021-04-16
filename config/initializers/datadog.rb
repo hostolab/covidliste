7098fc7266adc617949a183051f5dbeebc78520b
Mathieu Ripert <mathieu.ripert@gmail.com>
2021-04-16 16:50:33 +0200

    rbfix

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -2,4 +2,4 @@ Datadog.configure do |c|
   c.tracer enabled: Rails.env.production?
   c.use :rails, service_name: "covidliste"
 end
-Datadog.tracer.enabled =  Rails.env.production?
+Datadog.tracer.enabled = Rails.env.production?
