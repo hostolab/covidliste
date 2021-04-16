ed02274c1fea94ac633b0d6207d9fd008852de29
Mathieu Ripert <mathieu.ripert@gmail.com>
2021-04-16 17:00:12 +0200

    rbfix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,14 +24,12 @@ class ApplicationController < ActionController::Base
 
   def append_info_to_payload(payload)
     super
-    case 
-      when payload[:status] == 200
-        payload[:level] = "INFO"
-      when payload[:status] == 302
-        payload[:level] = "WARN"
-      else
-        payload[:level] = "ERROR"
+    payload[:level] = if payload[:status] == 200
+      "INFO"
+    elsif payload[:status] == 302
+      "WARN"
+    else
+      "ERROR"
     end
   end
-
 end
