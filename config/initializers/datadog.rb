a50c69d46f08bcadb6d1ed5dd4f3151782d9afb3
Mathieu Ripert <mathieu.ripert@gmail.com>
2021-04-16 15:33:26 +0200

    rbfix

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -2,8 +2,8 @@ Datadog.configure do |c|
   c.use :rails, service_name: "covidliste"
   c.tracer.transport_options = proc { |t|
     t.adapter :net_http,
-              Datadog::Transport::HTTP.default_hostname,
-              Datadog::Transport::HTTP.default_port,
-              { timeout: 30 }
+      Datadog::Transport::HTTP.default_hostname,
+      Datadog::Transport::HTTP.default_port,
+      {timeout: 30}
   }
 end
