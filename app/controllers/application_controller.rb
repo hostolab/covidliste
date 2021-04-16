23573a14e8b72213e6c6f94353ff9c5602af338d
Mathieu Ripert <mathieu.ripert@gmail.com>
2021-04-16 14:05:33 +0200

    lograge

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,4 +21,18 @@ class ApplicationController < ActionController::Base
   def skip_pundit?
     devise_controller?
   end
+
+  def append_info_to_payload(payload)
+    super
+    case 
+      when payload[:status] == 200
+        payload[:level] = "INFO"
+      when payload[:status] == 302
+        payload[:level] = "WARN"
+      else
+        payload[:level] = "ERROR"
+      end
+    end
+  end
+
 end
