ce40e33e675e8cc12085ee19052f9d23a03ce91d
Mathieu Ripert <mathieu.ripert@gmail.com>
2021-04-16 15:51:38 +0200

    trace disable

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,9 +1,4 @@
 Datadog.configure do |c|
+  c.tracer enabled: false
   c.use :rails, service_name: "covidliste"
-  c.tracer.transport_options = proc { |t|
-    t.adapter :net_http,
-      Datadog::Transport::HTTP.default_hostname,
-      Datadog::Transport::HTTP.default_port,
-      {timeout: 30}
-  }
 end
