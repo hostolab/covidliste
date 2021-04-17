383f60cb183f7cb6e56a31ea1781ffcec2afe6a3
Pierre Fournier <peiofour@gmail.com>
2021-04-17 14:21:00 +0200

    Add connect button in refused page to go removing account #365

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -10,7 +10,7 @@ class MatchesController < ApplicationController
 
   def destroy
     @match.refuse!
-    redirect_to root_path
+    redirect_back(fallback_location: root_path)
   end
 
   def update
