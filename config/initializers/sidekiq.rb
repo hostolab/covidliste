c3cb2f6427ac469674f3b4450599fb428e8a6297
Navid EMAD <navid.emad@yespark.fr>
2021-04-18 16:49:12 +0200

    Docker rewrite with all services and some fixes (#420)
    * Optimize docker setup
    
    * Remove extra testing logging
    
    * WIP
    
    * WIP
    
    * Small changes on the README.md
    
    * WIP adding test service
    
    * Add chrome driver to the Dockerfile for the tests + rework test env
    
    * WIP
    
    * Add datadog service in docker-compose + update readme with correct cmd for rspec
    
    * Remove dd agent docker-compose service and some cleaning
    
    Co-authored-by: Mathieu Ripert <mathieu.ripert@gmail.com>

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,9 @@
+if Gem::Version.new(Sidekiq::VERSION) < Gem::Version.new('6.1')
+  Redis.exists_returns_integer = true
+else
+  raise 'Time to remove Redis.exists_returns_integer: https://github.com/mperham/sidekiq/issues/4591'
+end
+
 Sidekiq.configure_client do |config|
   config.redis = {url: (ENV["REDIS_URL"] || "redis://localhost:6379"), size: 1}
 end
