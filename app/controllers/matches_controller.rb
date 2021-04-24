9c37fbbe68478e0e2d2773fc23761528f740311e
Romain Champourlier <public@rchampourlier.com>
2021-04-24 14:18:38 +0200

    Added column to monitor confirmation failures

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -25,6 +25,10 @@ class MatchesController < ApplicationController
   rescue Match::AlreadyConfirmedError, Match::DoseOverbookingError, Match::MissingNamesError, ActiveRecord::RecordInvalid => e
     flash.now[:error] = e.message
     @match.reload
+
+    # Updating the match's confirmation_failed_at to indicate it failed.
+    # This should help with detecting critical bugs.
+    @match.update_column(:confirmation_failed_at, Time.now.utc)
   ensure
     render action: "show", status: flash[:error].present? ? :unprocessable_entity : :ok
   end
