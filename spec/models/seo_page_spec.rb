f268af6af303c408b091e3da5ea34305e50d991a
navidemad <navid.emad@yespark.fr>
2021-04-24 22:49:06 +0200

    Fix SEO meta in the head and some spec

--- spec/models/seo_page_spec.rb
+++ spec/models/seo_page_spec.rb
@@ -1,5 +1,11 @@
 require "rails_helper"
 
 RSpec.describe SeoPage, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:seo_page) { build(:seo_page) }
+
+  describe "Fields validation" do
+    it "has a valid factory" do
+      expect(seo_page).to be_valid
+    end
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,7 @@ require "rspec/rails"
 require "devise"
 require "database_cleaner/active_record"
 require "webmock/rspec"
+require "action_text/system_test_helper"
 
 WebMock.disable_net_connect!(allow_localhost: true)
 
@@ -47,6 +48,7 @@ RSpec.configure do |config|
   config.include ActiveSupport::Testing::TimeHelpers
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include ActionView::RecordIdentifier, type: :system # to use the dom_id helper
+  config.include ActionText::SystemTestHelper, type: :system # to use fill_in_rich_text_area
   config.include SystemSpecHelpers, type: :system
   config.include Warden::Test::Helpers, type: :system
   config.after :each, type: :system do
