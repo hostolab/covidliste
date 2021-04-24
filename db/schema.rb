85f2683337a0c60684bb885675d51d19051c9992
Romain Champourlier <public@rchampourlier.com>
2021-04-24 16:38:17 +0200

    Updated schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_23_073642) do
+ActiveRecord::Schema.define(version: 2021_04_24_105052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -154,6 +154,7 @@ ActiveRecord::Schema.define(version: 2021_04_23_073642) do
     t.datetime "refused_at"
     t.datetime "email_first_clicked_at"
     t.datetime "sms_first_clicked_at"
+    t.datetime "confirmation_failed_at"
     t.index ["campaign_batch_id"], name: "index_matches_on_campaign_batch_id"
     t.index ["campaign_id"], name: "index_matches_on_campaign_id"
     t.index ["match_confirmation_token_bidx"], name: "index_matches_on_match_confirmation_token_bidx", unique: true
@@ -226,8 +227,8 @@ ActiveRecord::Schema.define(version: 2021_04_23_073642) do
     t.string "city"
     t.string "geo_citycode"
     t.string "geo_context"
-    t.boolean "statement", default: false
     t.datetime "anonymized_at"
+    t.boolean "statement", default: false
     t.datetime "statement_accepted_at"
     t.datetime "toc_accepted_at"
     t.index ["city"], name: "index_users_on_city"
