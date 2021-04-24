057b789458e3f6b54b2561e2cef215a2002901a7
Romain Champourlier <public@rchampourlier.com>
2021-04-24 16:32:10 +0200

    Replaced `timestamp` with `datetime` in migration

--- db/migrate/20210424105052_add_confirmation_failed_at_to_matches.rb
+++ db/migrate/20210424105052_add_confirmation_failed_at_to_matches.rb
@@ -1,5 +1,5 @@
 class AddConfirmationFailedAtToMatches < ActiveRecord::Migration[6.1]
   def change
-    add_column :matches, :confirmation_failed_at, :timestamp
+    add_column :matches, :confirmation_failed_at, :datetime
   end
 end
