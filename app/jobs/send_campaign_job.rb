3a9680121671a8289a4e102c91266007192b4eb1
Mathieu Ripert <mathieu.ripert@gmail.com>
2021-04-25 18:50:59 +0200

    standard

--- app/jobs/send_campaign_job.rb
+++ app/jobs/send_campaign_job.rb
@@ -6,7 +6,7 @@ class SendCampaignJob < ApplicationJob
   def perform(campaign, partner = nil)
     return unless campaign.running?
     return campaign.completed! if campaign.remaining_slots <= 0 || (campaign.ends_at - STOP_SENDING_BEFORE_CAMPAIGN_ENDS_AT) < Time.now.utc
-    
+
     limit = (campaign.remaining_slots * Vaccine.overbooking_factor(campaign.vaccine_type)).floor
     users = campaign.reachable_users_query(limit: limit)
 
