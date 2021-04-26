fb4aad9529fb52a6f1d6b5df8c414ab1f018b048
navidemad <navid.emad@yespark.fr>
2021-04-26 22:01:28 +0200

    Autolog on partner confirm too + redirect when correct page when already confirmed

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -4,22 +4,24 @@ class ConfirmationsController < ::Devise::ConfirmationsController
     yield resource if block_given?
     if resource.errors.empty?
       set_flash_message!(:notice, :confirmed)
-
-      # PasswordLess specific behaviour after confirmation there is a direct sign in
-      sign_in(resource) if resource.is_a?(User)
-
-      respond_with_navigational(resource) { redirect_to after_confirmation_path_for(resource_name, resource) }
+      sign_in(resource)
+      respond_with_navigational(resource) do
+        redirect_to after_confirmation_path_for(resource_name, resource)
+      end
     elsif resource.confirmed?
       set_flash_message!(:notice, :already_confirmed)
-      respond_with_navigational(resource.errors, status: :unprocessable_entity) { redirect_to resource.is_a?(Partner) ? :new_partner_session : :new_user_session }
+      respond_with_navigational(resource.errors, status: :unprocessable_entity) do
+        redirect_to resource.is_a?(Partner) ? :new_partner_session : :new_user_session, status: :unprocessable_entity
+      end
     else
-      respond_with_navigational(resource.errors, status: :unprocessable_entity) { render :new }
+      respond_with_navigational(resource.errors, status: :unprocessable_entity) do
+        render :new, status: :unprocessable_entity
+      end
     end
   end
 
   def after_confirmation_path_for(_resource_name, resource)
     if resource.is_a?(Partner)
-      # NOTE(ssaunier): We already have a valid password at sign up
       partners_vaccination_centers_path
     elsif resource.is_a?(User)
       profile_path
