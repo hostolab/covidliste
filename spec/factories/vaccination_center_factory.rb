293db3a226ffdd9e8a8d573c05dc1f1838c8ba47
Navid EMAD <navid.emad@yespark.fr>
2021-04-27 15:37:51 +0200

    Add column distance_in_meters into matches when match is created to cache user lat/lon delta with center (#612)

--- spec/factories/vaccination_center_factory.rb
+++ spec/factories/vaccination_center_factory.rb
@@ -5,14 +5,16 @@ FactoryBot.define do
     kind { VaccinationCenter::Kinds::ALL.sample }
     name { generate(:company_name) }
     phone_number { generate(:french_phone_number) }
+    lat { Faker::Address.latitude }
+    lon { Faker::Address.longitude }
 
-    trait :from_lyon do
+    trait :from_paris do
       address { "21 Rue Bergère 75009 Paris" }
       lat { "48.87242501471677" }
       lon { "2.344941896580627" }
     end
 
-    trait :from_paris do
+    trait :from_lyon do
       address { "7 Rue Auguste Comte 69002 Lyon" }
       lat { "45.75620064462772" }
       lon { "4.8319385046869945" }
