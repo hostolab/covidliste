de25d1f3213a5e64fa25cab4ac151696f91a9bc2
Mathieu Ripert <mathieu.ripert@gmail.com>
2021-04-30 09:29:14 +0200

    Add lockable to partners

---
+++ db/migrate/20210430072418_add_lockable_to_partners.rb
@@ -0,0 +1,10 @@
+class AddLockableToPartners < ActiveRecord::Migration[6.1]
+  def change
+    add_column :partners, :failed_attempts, :integer, default: 0, null: false # Only if lock strategy is :failed_attempts
+    add_column :partners, :locked_at, :datetime
+
+    # Add these only if unlock strategy is :email or :both
+    add_column :partners, :unlock_token, :string
+    add_index :partners, :unlock_token, unique: true
+  end
+end
