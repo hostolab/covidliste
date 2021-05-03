1e2df1ba8a85566c6d09d10dabf377225a7010fc
Coline Juillerat <68182973+colinemarie@users.noreply.github.com>
2021-05-03 19:33:10 +0200

    Update to email all partners

--- app/mailers/vaccination_center_mailer.rb
+++ app/mailers/vaccination_center_mailer.rb
@@ -2,15 +2,17 @@ class VaccinationCenterMailer < ApplicationMailer
   default from: "Covidliste <partenaires@covidliste.com>"
 
   def confirmed_vaccination_center_onboarding
-    @vaccination_center = params[:vaccination_center]
-    partner_email = @vaccination_center&.partners&.first&.email
     @tutorial_url = "https://docs.google.com/document/d/10IqYk739dJImngTIsQ7IJbf7RXJRG7drD4dZM_Z4xmI/edit?usp=sharing"
+    @vaccination_center = params[:vaccination_center]
+    partners = @vaccination_center&.partners
 
-    return if partner_email.blank?
+    partners.each do |partner|
+      next if partner.email.blank?
 
-    mail(
-      to: partner_email,
-      subject: "Covidliste - validation de votre lieu de vaccination"
-    )
+      mail(
+        to: partner.email,
+        subject: "Covidliste - validation de votre lieu de vaccination"
+      )
+    end
   end
 end
