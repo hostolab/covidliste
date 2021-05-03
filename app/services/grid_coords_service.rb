c11239effe048f7b87bcfa34eaf608443d9c2575
Mathieu Ripert <mathieu.ripert@gmail.com>
2021-05-03 14:37:29 +0200

    Add parameters to campaigns (#712)
    * Add parameters to campaigns
    
    * standard

--- app/services/grid_coords_service.rb
+++ app/services/grid_coords_service.rb
@@ -7,11 +7,11 @@ class GridCoordsService
   end
 
   def get_cell
-    lat_rad = @lat/180 * Math::PI
-    n = 2.0 ** DEFAULT_ZOOM_LEVEL
+    lat_rad = @lat / 180 * Math::PI
+    n = 2.0**DEFAULT_ZOOM_LEVEL
     i = ((@lon + 180.0) / 360.0 * n).to_i
-    j = ((1.0 - Math::log(Math::tan(lat_rad) + (1 / Math::cos(lat_rad))) / Math::PI) / 2.0 * n).to_i
+    j = ((1.0 - Math.log(Math.tan(lat_rad) + (1 / Math.cos(lat_rad))) / Math::PI) / 2.0 * n).to_i
 
-    {:i => i, :j => j}
+    {i: i, j: j}
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_02_203302) do
+ActiveRecord::Schema.define(version: 2021_05_03_122404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -123,6 +123,7 @@ ActiveRecord::Schema.define(version: 2021_05_02_203302) do
     t.integer "status", default: 0
     t.datetime "canceled_at"
     t.string "algo_version"
+    t.jsonb "parameters"
     t.index ["partner_id"], name: "index_campaigns_on_partner_id"
     t.index ["status"], name: "index_campaigns_on_status"
     t.index ["vaccination_center_id"], name: "index_campaigns_on_vaccination_center_id"
