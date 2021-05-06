89eae7b44914ddc22fe123fdb249c335472551bd
Coline Juillerat <coline.ju@gmail.com>
2021-05-06 19:21:42 +0200

    add back button

--- app/javascript/components/toggle_mobile_match_information.js
+++ app/javascript/components/toggle_mobile_match_information.js
@@ -1,12 +1,17 @@
 const toggleMobileMatchInformation = () => {
   const matchInfo = document.getElementById("mobile-match-info");
   const matchIdentity = document.getElementById("mobile-match-identity");
-  const buttonInterested = document.getElementById("interested-button");
+  const interestedButton = document.getElementById("interested-button");
+  const backButton = document.getElementById("mobile-match-back")
 
   if (matchInfo) {
-    buttonInterested.addEventListener("click", function (e) {
+    interestedButton.addEventListener("click", function (e) {
       matchInfo.classList.add("d-none");
       matchIdentity.classList.remove("d-none", "d-sm-block");
+    });
+    backButton.addEventListener("click", function(e) {
+      matchInfo.classList.remove("d-none");
+      matchIdentity.classList.add("d-none", "d-sm-block");
     });
   }
 };
