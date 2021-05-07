514daec4813347ce225a4eede1c27d09f8fdddbb
Maxence Aïci <maxence.aici@alan.eu>
2021-05-07 08:45:01 +0200

    Allow 08 phone numbers for vaccination centers

--- app/models/vaccination_center.rb
+++ app/models/vaccination_center.rb
@@ -1,6 +1,6 @@
 class VaccinationCenter < ApplicationRecord
   include HasPhoneNumberConcern
-  has_phone_number_types %i[fixed_line mobile voip]
+  has_phone_number_types %i[fixed_line mobile voip premium_rate]
   module Kinds
     CABINET_INFIRMIER = "Cabinet infirmier"
     CABINET_MEDICAL = "Cabinet médical"
