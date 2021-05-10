533a57e78f8c6da7113b63e1a9621edd9d358b1d
poudro <antoine@poudro.com>
2021-05-10 09:14:03 +0200

    Fix grid query (#729)
    * init: update queries to use new grid query format
    
    * chore: update tests
    
    * chore: remove now useless lat/lon query params
    
    * chore: quality
    
    * chore: sonar
    
    Co-authored-by: Antoine Sinton <antoine@cnty.ai>
    Co-authored-by: Mathieu Ripert <mathieu.ripert@gmail.com>

--- spec/services/reachable_users_service.rb
+++ spec/services/reachable_users_service.rb
@@ -8,13 +8,16 @@ def get_lat_lng_for_number(xtile, ytile, zoom)
   {lat: lat_deg, lon: lon_deg}
 end
 
+def get_rand_email
+  (0...8).map { rand(65..90).chr }.join + "@covidliste.com"
+end
+
 DEFAULT_ZOOM_LEVEL = 15
 
 RSpec.describe ReachableUsersService, type: :service do
   let!(:vaccination_center) { create(:vaccination_center, lat: 42, lon: 2) }
   let!(:campaign) { build(:campaign, vaccination_center: vaccination_center, max_distance_in_meters: 5000, min_age: 18, max_age: 130, available_doses: 10) }
   let!(:reachable_users_service) { ReachableUsersService.new(campaign) }
-  email_domain = "covidliste.com"
 
   describe "get_vaccination_center_grid_cells" do
     it "covering is valid" do
@@ -22,7 +25,9 @@ RSpec.describe ReachableUsersService, type: :service do
       expect(range[:center_cell][:i]).to eq(16566)
       expect(range[:center_cell][:j]).to eq(12164)
       expect(range[:dist_cells]).to eq(6)
-      expect(range[:cells].length).to eq(113)
+
+      expect(range[:cells_i]).to eq([16560, 16561, 16562, 16563, 16564, 16565, 16566, 16567, 16568, 16569, 16570, 16571, 16572])
+      expect(range[:cells_j]).to eq([12158, 12159, 12160, 12161, 12162, 12163, 12164, 12165, 12166, 12167, 12168, 12169, 12170])
 
       lat_lon_center = get_lat_lng_for_number(range[:center_cell][:i], range[:center_cell][:j], DEFAULT_ZOOM_LEVEL)
       expect(lat_lon_center[:lat]).to be_within(0.01).of(42)
@@ -35,7 +40,7 @@ RSpec.describe ReachableUsersService, type: :service do
       User.create(
         firstname: Faker::Name.first_name,
         lastname: Faker::Name.last_name,
-        email: Faker::Internet.unique.email(domain: email_domain),
+        email: get_rand_email,
         birthdate: Faker::Date.between(from: 100.years.ago, to: 55.years.ago),
         address: Faker::Address.full_address,
         phone_number: Faker::PhoneNumber.cell_phone_in_e164,
@@ -62,7 +67,7 @@ RSpec.describe ReachableUsersService, type: :service do
       User.create(
         firstname: Faker::Name.first_name,
         lastname: Faker::Name.last_name,
-        email: Faker::Internet.unique.email(domain: email_domain),
+        email: get_rand_email,
         birthdate: Faker::Date.between(from: 100.years.ago, to: 55.years.ago),
         address: Faker::Address.full_address,
         phone_number: Faker::PhoneNumber.cell_phone_in_e164,
@@ -81,25 +86,23 @@ RSpec.describe ReachableUsersService, type: :service do
 
   describe "get_users_with_v2 should find users" do
     before do
-      (1..5).each do |i|
-        User.create(
-          firstname: Faker::Name.first_name,
-          lastname: Faker::Name.last_name,
-          email: Faker::Internet.unique.email(domain: email_domain),
-          birthdate: Faker::Date.between(from: 100.years.ago, to: 55.years.ago),
-          address: Faker::Address.full_address,
-          phone_number: Faker::PhoneNumber.cell_phone_in_e164,
-          toc: true,
-          statement: true,
-          confirmed_at: Time.now.utc,
-          lat: 42,
-          lon: 2
-        )
-      end
       User.create(
         firstname: Faker::Name.first_name,
         lastname: Faker::Name.last_name,
-        email: Faker::Internet.unique.email(domain: email_domain),
+        email: get_rand_email,
+        birthdate: Faker::Date.between(from: 100.years.ago, to: 55.years.ago),
+        address: Faker::Address.full_address,
+        phone_number: Faker::PhoneNumber.cell_phone_in_e164,
+        toc: true,
+        statement: true,
+        confirmed_at: Time.now.utc,
+        lat: 42,
+        lon: 2
+      )
+      User.create(
+        firstname: Faker::Name.first_name,
+        lastname: Faker::Name.last_name,
+        email: get_rand_email,
         birthdate: Faker::Date.between(from: 100.years.ago, to: 55.years.ago),
         address: Faker::Address.full_address,
         phone_number: Faker::PhoneNumber.cell_phone_in_e164,
@@ -110,15 +113,50 @@ RSpec.describe ReachableUsersService, type: :service do
         lon: 1.93359375
       )
     end
-    it "get 5 users" do
+    it "get 1 user" do
       resp = reachable_users_service.get_users_with_v2(10)
 
-      expect(resp.length).to eq(5)
+      expect(resp.length).to eq(1)
 
       user = resp[0]
       # lat/lon randomisation can move cell around a bit
       expect(user.grid_i).to be_within(2).of(16565)
       expect(user.grid_j).to be_within(2).of(12163)
+    end
+  end
+
+  describe "get_users_count should find users" do
+    before do
+      User.create(
+        firstname: Faker::Name.first_name,
+        lastname: Faker::Name.last_name,
+        email: get_rand_email,
+        birthdate: Faker::Date.between(from: 100.years.ago, to: 55.years.ago),
+        address: Faker::Address.full_address,
+        phone_number: Faker::PhoneNumber.cell_phone_in_e164,
+        toc: true,
+        statement: true,
+        confirmed_at: Time.now.utc,
+        lat: 42,
+        lon: 2
+      )
+      User.create(
+        firstname: Faker::Name.first_name,
+        lastname: Faker::Name.last_name,
+        email: get_rand_email,
+        birthdate: Faker::Date.between(from: 100.years.ago, to: 55.years.ago),
+        address: Faker::Address.full_address,
+        phone_number: Faker::PhoneNumber.cell_phone_in_e164,
+        toc: true,
+        statement: true,
+        confirmed_at: Time.now.utc,
+        lat: 42.01665183556824,
+        lon: 1.93359375
+      )
+    end
+    it "count 1 user" do
+      resp = reachable_users_service.get_users_count
+      expect(resp).to eq(1)
     end
   end
 end
