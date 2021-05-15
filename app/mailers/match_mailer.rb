c49511752d43467f2acd479c7acc887f58025ef6
Coline Juillerat <coline.ju@gmail.com>
2021-05-16 01:19:45 +0200

    update match mailer

--- app/mailers/match_mailer.rb
+++ app/mailers/match_mailer.rb
@@ -12,6 +12,17 @@ class MatchMailer < ApplicationMailer
     )
   end
 
+  def send_confirmed_match_details
+    @match = params[:match]
+    @vaccination_center = @match.vaccination_center
+    return if @match.user.blank?
+
+    mail(
+      to: @match.user.email,
+      subject: "Covidliste - Votre rendez-vous de vaccination est confirmé"
+    )
+  end
+
   def send_anonymisation_notice
     @match = params[:match]
     user_email = params[:user_email] || @match&.user&.email
