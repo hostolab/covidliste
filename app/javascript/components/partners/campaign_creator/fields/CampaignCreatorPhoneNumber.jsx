4ac3e570924c254c76eb81d4732a86a2f54f5e12
Arnaud Peich <arnaud.peich@gmail.com>
2021-05-17 00:19:31 +0200

    prettier

--- app/javascript/components/partners/campaign_creator/fields/CampaignCreatorPhoneNumber.jsx
+++ app/javascript/components/partners/campaign_creator/fields/CampaignCreatorPhoneNumber.jsx
@@ -4,28 +4,31 @@ import { CampaignCreatorField } from "components/partners/campaign_creator/field
 import { CampaignCreatorInput } from "components/partners/campaign_creator/fields/CampaignCreatorInput";
 
 export const CampaignCreatorPhoneNumber = ({
-  vaccinationCenterPhoneNumber
+  vaccinationCenterPhoneNumber,
 }) => {
-  const fieldName = "phoneNumber"
+  const fieldName = "phoneNumber";
   const { setFieldValue, values } = useFormikContext();
   return (
     <>
-      <CampaignCreatorField label="Numéro de téléphone à communiquer aux volontaires (facultatif)" name={fieldName}>
-        <CampaignCreatorInput
-          type="tel"
-          name={fieldName}
-          id={fieldName}
-        />
+      <CampaignCreatorField
+        label="Numéro de téléphone à communiquer aux volontaires (facultatif)"
+        name={fieldName}
+      >
+        <CampaignCreatorInput type="tel" name={fieldName} id={fieldName} />
       </CampaignCreatorField>
-      { vaccinationCenterPhoneNumber && values[fieldName] !== vaccinationCenterPhoneNumber && (
-        <button
-          className="btn btn-outline-primary btn-sm"
-          type="button"
-          onClick={() => setFieldValue(fieldName, vaccinationCenterPhoneNumber)}
-        >
-          Utiliser le numéro du centre de vaccination ({vaccinationCenterPhoneNumber})
-        </button>
-      ) }
+      {vaccinationCenterPhoneNumber &&
+        values[fieldName] !== vaccinationCenterPhoneNumber && (
+          <button
+            className="btn btn-outline-primary btn-sm"
+            type="button"
+            onClick={() =>
+              setFieldValue(fieldName, vaccinationCenterPhoneNumber)
+            }
+          >
+            Utiliser le numéro du centre de vaccination (
+            {vaccinationCenterPhoneNumber})
+          </button>
+        )}
     </>
   );
 };
