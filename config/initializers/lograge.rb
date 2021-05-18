5ca2f6b3d9ac2d892c1a12642f08a08052dd242c
Mathieu Ripert <mathieu.ripert@gmail.com>
2021-05-18 14:24:41 +0200

    lograge

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -4,8 +4,6 @@ Rails.application.configure do
   config.lograge.formatter = Lograge::Formatters::Json.new
   config.colorize_logging = true
 
-  config.lograge.logger = ActiveSupport::Logger.new(File.join(Rails.root, "log", "#{Rails.env}.log"))
-
   # This is useful if you want to log query parameters
   config.lograge.custom_options = lambda do |event|
     {
