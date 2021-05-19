a327e6a66cc8b55e073a483fbf2419328321136d
Navid EMAD <navid.emad@yespark.fr>
2021-05-19 17:28:14 +0200

    Redlock testing_mode assignment due to changelog change

--- config/initializers/redlock.rb
+++ config/initializers/redlock.rb
@@ -2,5 +2,5 @@ redis_url = (ENV["REDIS_URL"] || "redis://localhost:6379")
 REDIS_LOCK = Redlock::Client.new([redis_url])
 if Rails.env.test?
   require "redlock/testing"
-  REDIS_LOCK.testing_mode = :bypass
+  Redlock::Client.testing_mode = :bypass
 end
