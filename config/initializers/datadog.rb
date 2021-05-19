9b45d9acc5c3da2c26838799b6fc599a87a81ae7
Navid EMAD <navid.emad@yespark.fr>
2021-05-19 17:27:44 +0200

    Disable database in testing env

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,5 +1,7 @@
-require "ddtrace"
+unless Rails.env.test?
+  require "ddtrace"
 
-Datadog.configure do |c|
-  c.use :rails, log_injection: true
+  Datadog.configure do |c|
+    c.use :rails, log_injection: true
+  end
 end
