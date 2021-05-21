a8e3a9aa5e56d9734e61bdc35758b268070757bb
Germain Carré <git@germain.sh>
2021-05-21 19:56:55 +0200

    Changing email validation library (mandatory for OpenID)

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -12,7 +12,7 @@ class Partner < ApplicationRecord
 
   validates :name, presence: true
   validates :phone_number, presence: true
-  validates :email, email: {mx: true, message: "Email invalide"}
+  validates :email, 'valid_email_2/email': {mx: true, message: "Email invalide"}
   validates :statement, presence: true, acceptance: true, unless: :reset_password_token?
 
   encrypts :email
