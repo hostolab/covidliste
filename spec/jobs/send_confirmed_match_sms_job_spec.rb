0ce7a819a6467962cc49bc77be78a2a709da380f
Paola Ducolin <paoletta84@hotmail.com>
2021-05-22 02:00:30 +0200

    ✅  [sms_confirm] add test for send confirmed match

---
+++ spec/jobs/send_confirmed_match_sms_job_spec.rb
@@ -0,0 +1,33 @@
+require "rails_helper"
+
+describe SendConfirmedMatchSmsJob do
+  let!(:user) { create(:user) }
+  let!(:campaign) { create(:campaign) }
+  let!(:match) { create(:match, user: user, campaign: campaign) }
+  let!(:twilio_mock) { double }
+  let!(:sendinblue_mock) { double }
+
+  match.confirm!
+
+  subject {
+    allow(Twilio::REST::Client).to receive(:new).and_return(twilio_mock)
+    allow(twilio_mock).to receive_message_chain(:messages, :create).and_return(double(sid: "smsid"))
+
+    allow(SibApiV3Sdk::TransactionalSMSApi).to receive(:new).and_return(sendinblue_mock)
+    allow(sendinblue_mock).to receive(:send_transac_sms).and_return(double(message_id: "smsid"))
+
+    SendConfirmedMatchSmsJob.new.perform(match.id)
+  }
+
+  context "match is confirmed by a user" do
+    it "sends the sms" do
+      expect(twilio_mock).to receive_message_chain(:messages, :create) # To update when flipping to sendinblue
+      subject
+    end
+
+    it "set confirm_sms_sent_at" do
+      subject
+      expect(match.reload.confirm_sms_sent_at).to_not be(nil)
+    end
+  end
+end
