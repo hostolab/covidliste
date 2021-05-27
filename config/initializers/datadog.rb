7da00167fff7dbdd0dc62f16766e30109d897e55
Guillaume Wrobel <20317297+GuillaumeWrobel@users.noreply.github.com>
2021-05-27 19:59:16 +0200

    Rendre la Description d'un centre de vaccination facultative (#887)
    * do not require description for parter/vaccination_center new
    
    * prettier datadog config hash spaces

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -2,7 +2,7 @@ unless Rails.env.test?
   require "ddtrace"
 
   Datadog.configure do |c|
-    c.use :rack, quantize: { query: { show: :all } }
+    c.use :rack, quantize: {query: {show: :all}}
     c.use :rails, log_injection: true
   end
 end
