e3ee1998b6a6f7f813fa44a4c9625e2226f0e59b
Mathieu Ripert <mathieu.ripert@gmail.com>
2021-05-28 22:57:04 +0200

    use sidekiq to send emails (#892)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,8 +58,9 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
   config.cache_store = :redis_cache_store, {url: ENV["REDIS_URL"]}
+
   # Use a real queuing backend for Active Job (and separate queues per environment).
-  # config.active_job.queue_adapter     = :resque
+  config.active_job.queue_adapter = :sidekiq
   # config.active_job.queue_name_prefix = "covidliste_production"
 
   config.action_mailer.perform_caching = false
