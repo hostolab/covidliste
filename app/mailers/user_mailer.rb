421712673d77b6bb1a7920e0a48331866eda59ca
Nicolas Zermati <nicoolas25@gmail.com>
2021-05-30 09:53:48 +0200

    Throttle inactive user emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,18 @@
 class UserMailer < ApplicationMailer
   default from: "Covidliste <inscription@covidliste.com>"
 
-  def send_inactive_user_unsubscription_request
-    user = User.find(params[:user_id])
+  MIN_SEND_INTERVAL = 7.days
 
-    return if user.email.blank?
+  def send_inactive_user_unsubscription_request
+    @user = User.find(params[:user_id])
+
+    return if @user.email.blank?
+    return if @user.last_inactive_user_email_sent_at && @user.last_inactive_user_email_sent_at >= MIN_SEND_INTERVAL.ago
+
+    @user.touch(:last_inactive_user_email_sent_at)
 
     mail(
-      to: user.email,
+      to: @user.email,
       subject: "Attendez-vous toujours une dose de vaccin ?"
     )
   end
