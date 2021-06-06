a9aee3e60d64e68007c6710811c20a8c44c6edc4
Germain Carré <git@germain.sh>
2021-06-07 01:32:44 +0200

    New baseline and update matches list on profile (#954)

--- spec/system/home_spec.rb
+++ spec/system/home_spec.rb
@@ -3,6 +3,6 @@ require "rails_helper"
 RSpec.describe "Home Page", type: :system do
   it "loads properly" do
     visit "/"
-    expect(page).to have_text("Accélérons la campagne de vaccination.")
+    expect(page).to have_text("Un vaccin rapidement, sans chercher.")
   end
 end
