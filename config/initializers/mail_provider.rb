46d653af513f7481d2fb376c6d26a899ef85199c
Germain Carré <git@germain.sh>
2021-06-07 18:57:10 +0200

    Adding postmark gem & config

--- config/initializers/mail_provider.rb
+++ config/initializers/mail_provider.rb
@@ -2,4 +2,4 @@ require "sib-api-v3-sdk"
 SibApiV3Sdk.configure do |config|
   config.api_key["api-key"] = ENV["SENDINBLUE_API_KEY"]
 end
-::PostmarkClient = Postmark::ApiClient.new(ENV["POSTMARK_API_TOKEN"])
+Rails.application.config.postmark_api_key = ENV["POSTMARK_API_TOKEN"]
