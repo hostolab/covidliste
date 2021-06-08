7836983015af9d76e17c5841afd8ea5264f703fd
Mathieu Ripert <mathieu.ripert@gmail.com>
2021-06-08 09:17:15 +0200

    [SlotsAlertService] check last slots (#961)

--- app/services/slot_alert_service.rb
+++ app/services/slot_alert_service.rb
@@ -6,18 +6,19 @@ class SlotAlertService
   end
 
   def call
-    slots.each do |slot|
+    slots.group_by { |x| x.center_id }.each do |center, slots|
+      slot = slots.first
       SendAlertsForSlotJob.perform_later({slot_id: slot.id, user_alerting_intensity: @user_alerting_intensity})
     end
   end
 
   def slots
     slots = VmdSlot
-      .where("created_at >= ?", 6.minutes.ago)
+      .where("created_at >= ?", 11.minutes.ago)
       .where("(pfizer is true or moderna is true) and astrazeneca is false")
     slots = slots.where("slots_1_days >= ?", @threshold) if @days <= 1
     slots = slots.where("slots_2_days >= ?", @threshold) if @days <= 2
     slots = slots.where("slots_7_days >= ?", @threshold) if @days <= 7
-    slots
+    slots.order("created_at desc")
   end
 end
