0dbb95e1291a7786fb7e36bfda2b738bfaa2f29b
Mathieu Ripert <mathieu.ripert@gmail.com>
2021-12-06 14:36:44 +0100

    [VMD] Fix VMD service

--- app/services/vmd_parser_service.rb
+++ app/services/vmd_parser_service.rb
@@ -4,17 +4,20 @@ class VmdParserService
 
   def parse
     slots.each do |slot|
-      VmdSlot.build_from_hash(slot)
+      VmdSlot.build_from_hash(slot, last_updated_at)
     end
+  end
+
+  def slots
+    @slots ||= data["centres_disponibles"]
+  end
+
+  def last_updated_at
+    @last_updated_at ||= data["last_updated"].to_datetime
   end
 
   def data
     @data ||= self.class.get("/info_centres.json")
   end
 
-  def slots
-    @centres ||= data.map do |k, v|
-      v["centres_disponibles"]
-    end.flatten
-  end
 end
